e03e9ffb3f01eb75e8b46990dfe30659ca966bc3
Reto Nüesch Erismann <91745230+nueesret@users.noreply.github.com>
2023-05-15 21:39:49 +0200

    Update server.js
    Text angepasst

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello DevOps Course :-)');
+res.send('Sally's gone home, hasn't she?');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
