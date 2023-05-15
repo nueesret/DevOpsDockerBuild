481e69fab6ca49f4b1e9c8252f7a9b9061760b86
Reto Nüesch Erismann <91745230+nueesret@users.noreply.github.com>
2023-05-15 23:33:38 +0200

    Update server.js
    Text angepasst

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('It is already dark outside!');
+res.send('Diese Woche war die Übungsaufgabe speziell schwierig, da die Tonspur in der Aufnahme fehlte!');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
